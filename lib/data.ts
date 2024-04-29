4f13481a08f61ff70362eb605f848f4b7fcfbd5f
Yohannes Seffefe <yohannesseffefe@gmail.com>
2024-04-29 18:14:39 +0300

    01 code

---
+++ lib/data.ts
@@ -0,0 +1,52 @@
+const data={
+    products:[{
+        name:'Free Shirt',
+        slug:'free-shirt',
+        category:'Shirts',
+        image:'/images/shirt1.jpg',
+        price:70,
+        brand:'Nike',
+        rating:4.5,
+        numReviews:8,
+        countInStock:20,
+        description:'A popular shirt',
+        isFeatured:true,
+        banner:'/images/banner1.jpg'
+
+    },
+    {
+        name:'Fit Shirt',
+        slug:'fit-shirt',
+        category:'Shirts',
+        image:'/images/shirt2.jpg',
+        price:80,
+        brand:'Adidas',
+        rating:3.2,
+        numReviews:10,
+        countInStock:20,
+        description:'A popular shirt',
+        isFeatured:true,
+        banner:'/images/banner2.jpg'
+
+    },
+
+    {
+        name:'Slim Shirt',
+        slug:'slim-shirt',
+        category:'Shirts',
+        image:'/images/shirt3.jpg',
+        price:90,
+        brand:'Raymond',
+        rating:4.5,
+        numReviews:3,
+        countInStock:20,
+        description:'A popular shirt',
+        isFeatured:true,
+        banner:'/images/banner2.jpg'
+
+    },
+
+],
+
+}
+export default data
